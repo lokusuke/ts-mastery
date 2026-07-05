3d65d1d97f7ce75a753a259e83b2d97b0dc924f0
lokusuke <suzloku63@gmail.com>
2026-07-05 20:07:22 +0900

    chap2-7まで

---
+++ src/chap02/index.ts
@@ -0,0 +1,18 @@
+// -- 基本の型と型注釈 --
+
+//string
+const message: string = "Hello, TypeScript!";
+
+const greeting: string = `メッセージ: ${message}`;
+console.log(greeting);
+
+// number
+const integer: number = 10; //整数
+const decimal: number = 3.14; //少数
+const binary: number = 0b1010; // 2進数
+
+console.log(integer + binary);
+
+// boolean
+const isActive: boolean = true;
+console.log(isActive);
